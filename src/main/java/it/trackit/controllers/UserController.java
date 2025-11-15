bee48ef4c15807cd55f13175ac62d6480a3d0919
Enrico Bregoli <bregolienrico@gmail.com>
2025-11-15 15:25:12 +0100

    Users: aggiunta prima versione dell'entity, controller, service, repository
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

---
+++ src/main/java/it/trackit/controllers/UserController.java
@@ -0,0 +1,23 @@
+package it.trackit.controllers;
+
+import it.trackit.controllers.services.UserService;
+import it.trackit.entities.User;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+  private final UserService userService;
+
+  @GetMapping
+  public List<User> getAllUsers() {
+    return userService.getAllUsers();
+  }
+}
---
+++ src/main/java/it/trackit/entities/User.java
@@ -0,0 +1,33 @@
+package it.trackit.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "password")
+  private String password;
+
+  @Column(name = "is_super_admin")
+  private Boolean isSuperAdmin;
+
+  @Column(name = "is_active")
+  private Boolean isActive;
+}
