9fbd76df8013de9d49ee1ffc238f34ee1bd4ad66
Enrico Bregoli <bregolienrico@gmail.com>
2025-11-16 17:56:50 +0100

    users: refactor del codice per usare il mapper per mappare le RegisterUserRequest e le UpdateUserRequest

--- src/main/java/it/trackit/mappers/UserMapper.java
+++ src/main/java/it/trackit/mappers/UserMapper.java
@@ -1,11 +1,18 @@
 package it.trackit.mappers;
 
+import it.trackit.dto.RegisterUserRequest;
+import it.trackit.dto.UpdateUserRequest;
 import it.trackit.dto.UserDto;
 import it.trackit.entities.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
   UserDto toDto(User user);
+  User toEntity(RegisterUserRequest request);
+
+  // il valore nullValuePropertyMappingStrategy dice a mapstruct di ignorare
+  // i campi null della source e di lasciare così come sono i campi del target
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+  void update(UpdateUserRequest request, @MappingTarget User user);
 }
--- src/main/java/it/trackit/services/UserService.java
+++ src/main/java/it/trackit/services/UserService.java
@@ -29,13 +29,9 @@ public class UserService {
   }
 
   public UserDto registerUser(RegisterUserRequest request) {
-    var newUser = User.builder()
-            .username(request.getUsername())
-            .email(request.getEmail())
-            .password(request.getPassword())
-            .isActive(true)
-            .isSuperAdmin(false)
-            .build();
+    var newUser = userMapper.toEntity(request);
+    newUser.setIsActive(true);
+    newUser.setIsSuperAdmin(false);
 
     userRepository.save(newUser);
 
@@ -45,14 +41,16 @@ public class UserService {
   public UserDto updateUser(Long userId, UpdateUserRequest request) {
     var user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
 
-    if (request.getUsername() != null)
-      user.setUsername(request.getUsername());
-    if (request.getEmail() != null)
-      user.setEmail(request.getEmail());
-    if (request.getNome() != null)
-      user.setNome(request.getNome());
-    if (request.getCognome() != null)
-      user.setCognome(request.getCognome());
+//    if (request.getUsername() != null)
+//      user.setUsername(request.getUsername());
+//    if (request.getEmail() != null)
+//      user.setEmail(request.getEmail());
+//    if (request.getNome() != null)
+//      user.setNome(request.getNome());
+//    if (request.getCognome() != null)
+//      user.setCognome(request.getCognome());
+
+    userMapper.update(request, user);
 
     userRepository.save(user);
 
