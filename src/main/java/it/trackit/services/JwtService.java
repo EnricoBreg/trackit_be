9f1dd45a6f1e8c001e939c17c83c3c63af8e6666
Enrico Bregoli <bregolienrico@gmail.com>
2025-12-17 20:50:07 +0100

    JWT: aggiunta dipendenza libreria per variabili d'ambiente, aggiunto file di esempio .env.example
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

--- src/main/java/it/trackit/services/JwtService.java
+++ src/main/java/it/trackit/services/JwtService.java
@@ -2,12 +2,16 @@ package it.trackit.services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+
+  @Value("${spring.jwt.secret}")
+  private String secret;
 
   public String generateToken(String username) {
     final long tokenExpiration = 86400; // 1 day
@@ -16,7 +20,7 @@ public class JwtService {
       .subject(username)
       .issuedAt(new Date())
       .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-      .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+      .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
       .compact();
   }
 }
