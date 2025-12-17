55fd8d0fe291489c4a4e847f7632bd905e0e87b1
Enrico Bregoli <bregolienrico@gmail.com>
2025-12-17 21:39:50 +0100

    Auth: aggiunto endpoint per avere le informazione sull'utente
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

--- src/main/java/it/trackit/controllers/AuthController.java
+++ src/main/java/it/trackit/controllers/AuthController.java
@@ -2,6 +2,9 @@ package it.trackit.controllers;
 
 import it.trackit.dtos.JwtResponse;
 import it.trackit.dtos.LoginRequest;
+import it.trackit.dtos.UserDto;
+import it.trackit.mappers.UserMapper;
+import it.trackit.repositories.UserRepository;
 import it.trackit.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +23,8 @@ public class AuthController {
 
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
 
   @PostMapping("/login")
   public ResponseEntity<JwtResponse> login(
@@ -41,6 +47,21 @@ public class AuthController {
     var token = authHeader.replace("Bearer ", "");
 
     return jwtService.validateToken(token);
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> me() {
+    var authentication = SecurityContextHolder.getContext().getAuthentication();
+    var username = (String)authentication.getPrincipal();
+
+    var user = userRepository.findByUsername(username).orElse(null);
+    if (user == null) {
+      return ResponseEntity.notFound().build();
+    }
+
+    var userDto = userMapper.toDto(user);
+
+    return ResponseEntity.ok(userDto);
   }
 
   @ExceptionHandler({BadCredentialsException.class})
