1e220e876b6cb2fa7ad961d5ea6a01d9b3f95be7
Enrico Bregoli <bregolienrico@gmail.com>
2025-12-21 11:07:56 +0100

    Exceptions: aggiunto global exception handler per la gestione delle eccezioni e dei relativi messaggi di errore
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

---
+++ src/main/java/it/trackit/commons/exceptions/ErrorDto.java
@@ -0,0 +1,11 @@
+package it.trackit.commons.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorDto {
+  private String error;
+}
+
