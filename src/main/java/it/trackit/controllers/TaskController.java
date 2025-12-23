f0b76b89203abc5a76c8b8c460b3dad2e7daa781
Enrico Bregoli <bregolienrico@gmail.com>
2025-12-24 00:11:53 +0100

    TaskController: aggiunto scaffolding per i futuri metodi da implementare
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

--- src/main/java/it/trackit/controllers/TaskController.java
+++ src/main/java/it/trackit/controllers/TaskController.java
@@ -1,10 +1,31 @@
 package it.trackit.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import it.trackit.dtos.projects.TaskDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/tasks")
 public class TaskController {
-  
+
+  @GetMapping("/{taskId}")
+  public TaskDto getTask(@PathVariable("taskId") Long taskId) {
+    throw new UnsupportedOperationException();
+  }
+
+  @PutMapping("/{taskId}")
+  public TaskDto updateTask(@PathVariable("taskId") Long taskId, @RequestBody TaskDto taskDto) {
+    throw new UnsupportedOperationException();
+  }
+
+  @DeleteMapping("/{taskId}")
+  public void deleteTask(@PathVariable("taskId") Long taskId) {
+    throw new UnsupportedOperationException();
+  }
+
+  @PatchMapping("/{taskId}/assignee")
+  public TaskDto assignTask(@PathVariable("taskId") Long taskId, @RequestBody Long userId) {
+    throw new UnsupportedOperationException();
+  }
 }
