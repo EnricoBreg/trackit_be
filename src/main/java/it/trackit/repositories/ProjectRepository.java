83c4c7421531572f734bb8134a6f2c68927652db
Enrico Bregoli <bregolienrico@gmail.com>
2026-01-29 00:17:23 +0100

    Project: correzione repository
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

--- src/main/java/it/trackit/repositories/ProjectRepository.java
+++ src/main/java/it/trackit/repositories/ProjectRepository.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
-  @EntityGraph(attributePaths = {"tasks.assegnatario", "tasks.creatore"})
+  @EntityGraph(attributePaths = {"tasks.assegnatario", "tasks.reporter"})
   @Query("SELECT p FROM Project p WHERE p.id = :projectId")
   Optional<Project> getProjectWithTasks(@Param("projectId") UUID id);
 
