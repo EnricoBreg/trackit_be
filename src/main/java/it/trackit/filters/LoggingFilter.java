a4ba4a438649424b98a8c9db7d50586b7c36853d
Enrico Bregoli <bregolienrico@gmail.com>
2026-01-29 00:32:28 +0100

    LoggingFilter: aggiunto log del metodo della richiesta
    Signed-off-by: Enrico Bregoli <bregolienrico@gmail.com>

--- src/main/java/it/trackit/filters/LoggingFilter.java
+++ src/main/java/it/trackit/filters/LoggingFilter.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class LoggingFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    log.info("Request: {}", request.getRequestURI());
+    log.info("Request: {} {}", request.getMethod(), request.getRequestURI());
     try {
       filterChain.doFilter(request, response);
     } finally {
